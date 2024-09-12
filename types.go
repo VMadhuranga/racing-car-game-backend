ad7b148a5e8929f2b28d0f6ac30a5837b1648918
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-09-12 14:26:37 +0530

    Create apiConfig, userPayload, validationError, userValidationErrors types

---
+++ types.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"github.com/VMadhuranga/racing-car-game-backend/internal/database"
+	"github.com/go-playground/validator/v10"
+)
+
+type apiConfig struct {
+	queries  *database.Queries
+	validate *validator.Validate
+}
+
+type userPayload struct {
+	Username string `validate:"required"`
+	Password string `validate:"required,alphanum"`
+}
+
+type validationError struct {
+	field, tag string
+}
+
+type userValidationErrors struct {
+	Username []string `json:"username,omitempty"`
+	Password []string `json:"password,omitempty"`
+}
