b8de3d582666fea65d7e26c25d2b09869c82ffc2
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-09-12 14:26:21 +0530

    Create respondWithJson function

---
+++ respond-with-json.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func respondWithJson(w http.ResponseWriter, statusCode int, payload interface{}) {
+	response, err := json.Marshal(payload)
+
+	if err != nil {
+		log.Printf("Could not encode response: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+
+	if payload != nil {
+		w.Write(response)
+	}
+}
