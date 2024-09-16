8be14d71852d6ba904cca8275a4407577d5cc694
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-09-16 16:14:24 +0530

    Create userResponse type

--- types.go
+++ types.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/VMadhuranga/racing-car-game-backend/internal/database"
 	"github.com/go-playground/validator/v10"
+	"github.com/google/uuid"
 )
 
 type apiConfig struct {
@@ -26,4 +27,9 @@ type userValidationErrorResponse struct {
 	Username        []string `json:"username,omitempty"`
 	Password        []string `json:"password,omitempty"`
 	ConfirmPassword []string `json:"confirm-password,omitempty"`
+}
+
+type userResponse struct {
+	Id       uuid.UUID `json:"id,omitempty"`
+	Username string    `json:"username,omitempty"`
 }
