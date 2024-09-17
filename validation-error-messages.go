836cb9579027037c727a4881a056d91a60e13572
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-09-17 16:35:23 +0530

    Update validationErrorMessages

--- validation-error-messages.go
+++ validation-error-messages.go
@@ -13,4 +13,32 @@ var validationErrorMessages = map[validationError]string{
 		"Password",
 		"alphanum",
 	}: "Password must contain letters and numbers only",
+	{
+		"NewUsername",
+		"required",
+	}: "New user name is required",
+	{
+		"OldPassword",
+		"required",
+	}: "Old password is required",
+	{
+		"OldPassword",
+		"alphanum",
+	}: "Old password must contain letters and numbers only",
+	{
+		"NewPassword",
+		"required",
+	}: "New password is required",
+	{
+		"NewPassword",
+		"alphanum",
+	}: "New password must contain letters and numbers only",
+	{
+		"ConfirmNewPassword",
+		"required",
+	}: "Confirm new password is required",
+	{
+		"ConfirmNewPassword",
+		"alphanum",
+	}: "Confirm new password must contain letters and numbers only",
 }
