20ccca3a6e26c50760eed52104fe915500690801
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-09-24 11:46:55 +0530

    Create updateUserBestTimeById method

---
+++ handle-update-user-best-time-by-user-id.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/VMadhuranga/racing-car-game-backend/internal/database"
+	"github.com/go-chi/chi/v5"
+	"github.com/google/uuid"
+)
+
+func (api apiConfig) updateUserBestTimeByUserId(w http.ResponseWriter, r *http.Request) {
+	userId, err := uuid.Parse(chi.URLParam(r, "userId"))
+
+	if err != nil {
+		log.Printf("Error parsing userId: %s", err)
+		respondWithError(w, 400, "Error parsing userId")
+		return
+	}
+
+	_, err = api.queries.GetUserById(r.Context(), userId)
+
+	if err != nil {
+		log.Printf("Error getting user: %s", err)
+		respondWithError(w, 404, "Error getting user")
+		return
+	}
+
+	payload := updateBestTimePayLoad{}
+	err = json.NewDecoder(r.Body).Decode(&payload)
+
+	if err != nil {
+		log.Printf("Error decoding payload: %s", err)
+		respondWithError(w, 400, "Error decoding payload")
+		return
+	}
+
+	err = api.validate.Struct(payload)
+
+	if err != nil {
+		log.Printf("Error validating payload: %s", err)
+		respondWithError(w, 400, "Error validating payload")
+		return
+	}
+
+	err = api.queries.UpdateUserBestTimeByUserId(r.Context(), database.UpdateUserBestTimeByUserIdParams{
+		UserID:   userId,
+		BestTime: payload.BestTime,
+	})
+
+	if err != nil {
+		log.Printf("Error updating user best time: %s", err)
+		respondWithError(w, 500, "Error updating user best time")
+		return
+	}
+
+	respondWithJson(w, 204, nil)
+}
