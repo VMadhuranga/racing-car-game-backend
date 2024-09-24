10b7012f2c8e8aad40714e68c2d9fdd80847e8fb
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-09-24 11:46:05 +0530

    Define sql query to update user best time

--- sql/queries/leader-board.sql
+++ sql/queries/leader-board.sql
@@ -1,3 +1,6 @@
 -- name: AddUserToLeaderBoard :exec
 INSERT INTO leader_board (id, best_time, user_id)
 VALUES ($1, $2, $3);
+
+-- name: UpdateUserBestTimeByUserId :exec
+UPDATE leader_board SET best_time = $1 WHERE user_id = $2;
