ebaf800753cbfecbbaf4d25688c3081d1a83396b
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-09-25 12:01:45 +0530

    Define sql query to get leader board

--- sql/queries/leader-board.sql
+++ sql/queries/leader-board.sql
@@ -4,3 +4,9 @@ VALUES ($1, $2, $3);
 
 -- name: UpdateUserBestTimeByUserId :exec
 UPDATE leader_board SET best_time = $1 WHERE user_id = $2;
+
+-- name: GetLeaderBoard :many
+SELECT l.id, l.best_time, u.username
+FROM leader_board l
+JOIN users u ON l.user_id = u.id
+ORDER BY l.best_time;
