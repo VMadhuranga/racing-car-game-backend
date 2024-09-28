9817977baa934efbbcd57b2d84277fde468e8caf
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-09-28 11:48:59 +0530

    Use userSignInPayload type as payload type

--- handle-user-sign-in.go
+++ handle-user-sign-in.go
@@ -11,7 +11,7 @@ import (
 )
 
 func (api apiConfig) handleUserSignIn(w http.ResponseWriter, r *http.Request) {
-	var payload createUserPayload
+	var payload userSignInPayload
 	err := json.NewDecoder(r.Body).Decode(&payload)
 
 	if err != nil {
