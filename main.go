8dd06a1d2fd1d2827bf8d0913c484ff7dc58343a
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-09-28 19:50:40 +0530

    Update godotenv error handling

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ func main() {
 	err := godotenv.Load()
 
 	if err != nil {
-		log.Fatalf("Error loading .env file: %s", err)
+		log.Printf("Error loading .env file: %s", err)
 	}
 
 	dbUri := os.Getenv("POSTGRES_URI")
